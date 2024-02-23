76b53852b79df21e93cc8bdab79a69c47f2ff2db
Стас Басов <stas.basov@whatever.com>
2024-02-23 19:08:54 +0500

    adadada

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='False')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
