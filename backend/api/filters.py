59a123bc05bdc0ed40e4b6961fccbbb0b0fe2dca
Стас Басов <stas.basov@whatever.com>
2024-02-28 19:04:57 +0500

    af

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -29,10 +29,7 @@ class RecipeFilter(FilterSet):
 
 
 class IngredientFilter(FilterSet):
-    name = filters.CharFilter(
-        field_name='name',
-        lookup_expr='icontains',
-    )
+    name = filters.CharFilter(lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
